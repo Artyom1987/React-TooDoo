5cfd04c5ca7d246349f89dfcaa323a9cbfaffa4d
Artyom1 <vovchokartem@gmail.com>
2020-06-18 17:25:52 +0300

    CSS commit

--- src/components/app/Index.js
+++ src/components/app/Index.js
@@ -1,44 +1,48 @@
 import React, {useState} from 'react';
+import   "./Style.css"
 
 
 let initTasks = [
-    {id: 1,title: "Сходить в туалет",done:true },
-    {id: 2,title: "Почистить зубы",done:false},
-    {id: 3,title: "Попить воды",done:false},
-    {id: 4,title: "Позавтракать",done:false}
+    {id: 1, title: "Сходить в туалет", done: false},
+    {id: 2, title: "Почистить зубы", done: false},
+    {id: 3, title: "Попить воды", done: false},
+    {id: 4, title: "Позавтракать", done: false}
 ];
 
 
 const App = () => {
-    const [newTasks,setNewTasks] = useState("");
-    const [tasks,setTasks] = useState(initTasks);
+    const [newTasks, setNewTasks] = useState("");
+    const [tasks, setTasks] = useState(initTasks);
 
 
-    const taskChanges =(el) => {
-        setTasks(tasks.map(item => item.id === el.id ?{...item,done: !item.done}: item )) ;
+    const taskChanges = (el) => {
+        setTasks(tasks.map(item => item.id === el.id ? {...item, done: !item.done} : item));
     };
     const enterNewTask = (e) => setNewTasks(e.target.value);
-    const newTask = () =>{
+    const newTask = () => {
         // const taskObj = {id: tasks.length + 1,title:newTasks,done: false};
         // console.log(taskObj);
         // const newTaskArr = tasks.concat([taskObj]);
         // console.log(newTaskArr);
         // setTasks(newTaskArr);
         // setTasks(tasks.concat({id: tasks.length + 1,title:newTasks,done: false}));
-        setTasks([...tasks,{id: tasks.length + 1,title:newTasks,done: false}])
+        setTasks([...tasks, {id: tasks.length + 1, title: newTasks, done: false}])
         setNewTasks("");
     };
 
     return (
-        <div>
-            <ul className = {'task-list'} >
-                {
-                    tasks.map(el => <li key={el.id} onClick={() => taskChanges(el) }
-                                        className={el.done?'task-done':''}> {el.title}</li>)
-                }
-            </ul>
-            <div>
-                <input type="text" value={newTasks} onChange={enterNewTask} />
+        <div className = "container" >
+            <div className = "box" >
+                <ul className={'task-list'}>
+                    {
+                        tasks.map(el => <li key={el.id} onClick={() => taskChanges(el)}
+                                            className={el.done ? 'task-done' : ''}> {el.title}</li>)
+                    }
+                </ul>
+            </div>
+
+            <div className = "box" >
+                <input type="text" value={newTasks} onChange={enterNewTask}/>
                 <button onClick={newTask}> Add new Task</button>
             </div>
         </div>
